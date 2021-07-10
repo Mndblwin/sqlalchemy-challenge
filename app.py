dcd29fcbe2d1d624f0570ca2f1a1e8623e7c9556
kerry.olden@gmail.com <kerry.olden@gmail.com>
2021-07-09 20:45:08 -0500

    final flask

--- app.py
+++ app.py
@@ -46,44 +46,56 @@ def welcome():
     )
 
 
-@app.route("/api/v1.0/names")
-def names():
+@app.route("/api/v1.0/precipitation")
+def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
+    
+    # Query all precipitation and dates
+    results = session.query(newBase.measurement, newBase.date).all()
 
     session.close()
 
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
-
-    return jsonify(all_names)
+    return jsonify(results)
 
 
-@app.route("/api/v1.0/passengers")
-def passengers():
+@app.route("/api/v1.0/stations")
+def station():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+    """Return a list of all stations"""
+    # Query all stations
+    results = session.query(newBase.station).all()
 
     session.close()
+
+    return jsonify(results)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of Tobs"""
+    # Query all stations
+    results = session.query(newBase.station).all()
+
+    session.close()
+
 
     # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
+    all_weather = []
+    for precipitation, station, tobs in results:
+        weather_dict = {}
+        weather_dict["Precipitation"] = precipitation
+        weather_dict["station"] = station
+        weather_dict["tobs"] = tobs
+        all_weather.append(weather_dict)
 
-    return jsonify(all_passengers)
+    return jsonify(all_weather)
 
 
 if __name__ == '__main__':
